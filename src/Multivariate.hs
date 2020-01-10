413aa363893b1a00bf8dd054e57378385fa9d362
Dan Piponi <dpiponi@gmail.com>
2020-01-09 20:13:47 -0800

    Added missing Multivariate.hs

---
+++ src/Multivariate.hs
@@ -0,0 +1,18 @@
+module Multivariate where
+
+import Wattage as W
+import Homogeneous as H
+
+type Multivariate a = Formal (Homogeneous a)
+
+var :: (Show a, Num a) => Int -> Multivariate a
+var i = F [Zero, make_var i (i + 1)]
+
+coefficient :: (Eq a, Num a, Show a) => Exponent -> Multivariate a -> a
+coefficient is f = H.coefficient is (W.coefficient (sum is) f)
+
+integrate :: (Num a, Eq a, Show a, Fractional a) => Int -> Multivariate a -> Multivariate a
+integrate i xs = 0 `prepend` mapf (hint i) xs
+
+d :: (Num a, Eq a, Show a, Fractional a) => Int -> Multivariate a -> Multivariate a
+d i xs = 0 `prepend` mapf (hint i) xs
