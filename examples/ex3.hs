64b8b3f99ef3c8d3c60fa2afa9392ec6b7801758
Dan Piponi <dpiponi@gmail.com>
2020-01-10 21:35:04 -0800

    Small tweak

--- examples/ex3.hs
+++ examples/ex3.hs
@@ -15,4 +15,6 @@ main = do
   -- A054474 Number of walks on square lattice that start and end at
   -- origin after 2n steps, not touching origin at intermediate stages.
   -- http://oeis.org/A054474
+  -- # of valid terms doubles with each iteration so 10 is
+  -- good enough for 1000 terms.
   print $ F.truncate 1000 $ fst $ iterate 10 agm (f, g)
